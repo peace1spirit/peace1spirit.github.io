f91f13e076e2862f443aa88388444c373e2b21f0
peace1spirit <peace1spirit@gmail.com>
2018-08-04 09:02:41 +0700

    Update function.js

--- week2/function.js
+++ week2/function.js
@@ -1,3 +1,4 @@
+//SOAL NO 1
 function shoutOut(){
 
     return 'Halo Function'
@@ -5,6 +6,7 @@ function shoutOut(){
 
 console.log(shoutOut())
 
+//SOAL NO 2
 function calculateMultiply(num1,num2){
     return num1*num2
 }
@@ -15,10 +17,7 @@ var num2 = 6;
 var hasilPerkalian = calculateMultiply(num1,num2);
 console.log(hasilPerkalian); // Menampilkan angka 30
 
-
-
-
-
+//SOAL NO 3
 function processSentence(name,age,address,hobby){
     return 'Nama saya '+name+' , umur saya '+ age +' tahun, alamat saya di '+ address
      +', dan saya punya hobby yaitu '+hobby + '!'
